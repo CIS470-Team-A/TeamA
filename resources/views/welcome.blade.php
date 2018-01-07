0d29749d1e55b0ab78a28add0eda53909dc44ecf
Lacie Cumberland <Lacie@Bastet.ivalice.barrekroberts.com>
2018-01-07 16:29:01 -0600

    Change example

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    WELCOME Team A
+                    WELCOME Team A!!!!
                 </div>
 
                 <div class="links">
