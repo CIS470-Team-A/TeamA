d4c7fd8157a3213f51fe97a45f59b78cd601938b
tnichols86 <t.nichols86@gmail.com>
2018-02-15 21:07:29 -0500

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -69,6 +69,22 @@ Please continue to update this during the development of the site.
 				  <span class="dot" onclick="currentSlide(2)"></span> 
 				  <span class="dot" onclick="currentSlide(3)"></span> 
 				</div>
+				
+				<div>
+				<h4>Lorem ipsum dolor sit amet, est sanctus dolorem eu, sea at veri civibus luptatum, te mei ullum consetetur efficiantur. Pri unum omnesque ea, error conceptam est ex. Augue euismod phaedrum vis ea, integre mentitum at eam, cum ne tota denique. Modus assum delenit in pri. Sea id facilis indoctum, no inimicus suavitate sit, vitae graeco commodo nam ne. Utinam omnium vix ne, dicam legere appellantur vix id.
+
+Iriure legendos mediocrem eu est, ea eum case impetus patrioque, eum eu facilisi inciderint. Id vis tritani legimus dissentiet. Novum ludus legere id sed, usu dolor assueverit interpretaris ex. Eu qui saperet pertinax explicari, ne novum perfecto sit. At pro labore civibus, quo nemore vituperatoribus an.
+
+Ea cum saepe principes. Eum autem urbanitas assentior ne. No vis discere bonorum, eam ei quod denique. Vel ei fierent scaevola ocurreret. Latine omnesque pro eu, ut putant efficiantur vim. Ne doctus admodum intellegebat pro.
+
+Ne cum corpora consulatu appellantur. Eros consul inimicus mel ex. Quidam hendrerit ius id, duo ea putent aliquam. At case graeco nam. Id mel dicunt inimicus, eum ei vero quaerendum, at illum iracundia disputationi pro. Ne mea nulla dolorum dolorem, mei ea unum dicam habemus.
+
+Consul mediocrem laboramus ius no, eam zril delenit deserunt no. Affert deserunt principes mei ne, graecis deserunt nec ad. No cum odio recteque. Per iriure eripuit intellegat id, te case tantas eloquentiam usu. Ex nobis noster sea, nec no sumo alia.
+
+Pri ex summo putent delectus, novum consul sensibus ad has, mea ne dictas saperet. Te accumsan corrumpit gubergren vis. His numquam mediocritatem ea, hinc recusabo sea an, te cum elitr nostrud concludaturque. Omnium percipit reprehendunt sed ex, eu senserit complectitur ius, eu sed clita propriae interpretaris. Feugait noluisse ea sit. Dolorum menandri no per. At audire signiferumque necessitatibus vim, sed et dicit timeam evertitur.
+
+Dicam maiorum gubergren te his. Cetero insolens disputationi ut cum, gubergren assentior usu no, ad cum tation soleat scribentur. Quando nostrum ne quo. Ius iisque eripuit ut, ei ius tritani senserit dignissim.</h4>
+				</div>
             <!--- End Body code --->
 		</div>
         <!--- Footer --->
