075206bef83b671dc9bcac5b898f97c92f795b3c
Lacie Cumberland <Lacie@Bastet.ivalice.barrekroberts.com>
2018-02-17 17:04:24 -0600

    mydb update

--- myDB.sql
+++ myDB.sql
@@ -16,29 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `category`
---
-
-DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `category` (
-  `category_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `category`
---
-
-LOCK TABLES `category` WRITE;
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `customer`
 --
 
@@ -120,13 +97,14 @@ CREATE TABLE `lineitems` (
   `Product_Id` int(11) NOT NULL,
   `Quantity` int(11) NOT NULL,
   `Price` decimal(10,2) NOT NULL,
+  `Product_Content` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Id_UNIQUE` (`Id`),
   KEY `fk_LineItems_Order1_idx` (`Order_Id`),
   KEY `fk_LineItems_Products1_idx` (`Product_Id`),
   CONSTRAINT `fk_LineItems_Order1` FOREIGN KEY (`Order_Id`) REFERENCES `order` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_LineItems_Products1` FOREIGN KEY (`Product_Id`) REFERENCES `products` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -135,31 +113,8 @@ CREATE TABLE `lineitems` (
 
 LOCK TABLES `lineitems` WRITE;
 /*!40000 ALTER TABLE `lineitems` DISABLE KEYS */;
+INSERT INTO `lineitems` VALUES (9,9,1,1,15.99,NULL),(10,9,2,1,25.99,NULL),(11,9,3,1,39.99,NULL);
 /*!40000 ALTER TABLE `lineitems` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `migrations`
---
-
-DROP TABLE IF EXISTS `migrations`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `migrations` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `migrations`
---
-
-LOCK TABLES `migrations` WRITE;
-/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -173,12 +128,11 @@ CREATE TABLE `order` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Customer_Id` int(11) NOT NULL,
   `Status` varchar(45) NOT NULL,
-  `Product_Content` varchar(150) NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Id_UNIQUE` (`Id`),
   KEY `fk_Order_Customer1_idx` (`Customer_Id`),
   CONSTRAINT `fk_Order_Customer1` FOREIGN KEY (`Customer_Id`) REFERENCES `customer` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -187,7 +141,7 @@ CREATE TABLE `order` (
 
 LOCK TABLES `order` WRITE;
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
-INSERT INTO `order` VALUES (1,5,'Shipped','T-Shirt with Brewing Logos'),(2,5,'Processed','Sweater with Bar Design'),(3,5,'On Order','T-Shirt with Brewing Steps'),(4,6,'Processed','Small Plaque with Soldier Rank'),(5,7,'Shipped','Small Trophy for kids softball'),(6,8,'Shipped','Racing Team Jersey'),(7,8,'On Order','Medium Trophy with speed record'),(8,8,'On Order','Large Trophy with 1st place');
+INSERT INTO `order` VALUES (9,5,'Processing');
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -210,7 +164,7 @@ CREATE TABLE `payment` (
   KEY `fk_Payment_Order1_idx` (`Order_Id`),
   CONSTRAINT `fk_Payment_Customer1` FOREIGN KEY (`Customer_Id`) REFERENCES `customer` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_Payment_Order1` FOREIGN KEY (`Order_Id`) REFERENCES `order` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -219,7 +173,6 @@ CREATE TABLE `payment` (
 
 LOCK TABLES `payment` WRITE;
 /*!40000 ALTER TABLE `payment` DISABLE KEYS */;
-INSERT INTO `payment` VALUES (1,5,1,0,15.99),(2,5,2,0,25.99),(3,5,3,0,15.99),(4,6,4,0,25.99),(5,7,5,0,15.99),(6,8,6,0,59.99),(7,8,7,0,25.99),(8,8,8,0,39.99);
 /*!40000 ALTER TABLE `payment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -239,7 +192,7 @@ CREATE TABLE `products` (
   `ProductName` varchar(45) NOT NULL,
   `ProductPic` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -280,7 +233,7 @@ CREATE TABLE `shoppingcart` (
 
 LOCK TABLES `shoppingcart` WRITE;
 /*!40000 ALTER TABLE `shoppingcart` DISABLE KEYS */;
-INSERT INTO `shoppingcart` VALUES ('::1','default','O:29:\"Illuminate\\Support\\Collection\":1:{s:8:\"\0*\0items\";a:3:{s:32:\"370d08585360f5c568b18d1f2e4ca1df\";O:32:\"Gloudemans\\Shoppingcart\\CartItem\":8:{s:5:\"rowId\";s:32:\"370d08585360f5c568b18d1f2e4ca1df\";s:2:\"id\";i:2;s:3:\"qty\";s:1:\"1\";s:4:\"name\";s:13:\"Medium Trophy\";s:5:\"price\";d:25.99;s:7:\"options\";O:39:\"Gloudemans\\Shoppingcart\\CartItemOptions\":1:{s:8:\"\0*\0items\";a:0:{}}s:49:\"\0Gloudemans\\Shoppingcart\\CartItem\0associatedModel\";N;s:41:\"\0Gloudemans\\Shoppingcart\\CartItem\0taxRate\";i:21;}s:32:\"a775bac9cff7dec2b984e023b95206aa\";O:32:\"Gloudemans\\Shoppingcart\\CartItem\":8:{s:5:\"rowId\";s:32:\"a775bac9cff7dec2b984e023b95206aa\";s:2:\"id\";i:3;s:3:\"qty\";s:1:\"1\";s:4:\"name\";s:12:\"Large Trophy\";s:5:\"price\";d:39.99;s:7:\"options\";O:39:\"Gloudemans\\Shoppingcart\\CartItemOptions\":1:{s:8:\"\0*\0items\";a:0:{}}s:49:\"\0Gloudemans\\Shoppingcart\\CartItem\0associatedModel\";N;s:41:\"\0Gloudemans\\Shoppingcart\\CartItem\0taxRate\";i:21;}s:32:\"808821852042d8780b9f862c35c42c68\";O:32:\"Gloudemans\\Shoppingcart\\CartItem\":8:{s:5:\"rowId\";s:32:\"808821852042d8780b9f862c35c42c68\";s:2:\"id\";i:7;s:3:\"qty\";s:1:\"1\";s:4:\"name\";s:16:\"Large Embroidery\";s:5:\"price\";d:49.99;s:7:\"options\";O:39:\"Gloudemans\\Shoppingcart\\CartItemOptions\":1:{s:8:\"\0*\0items\";a:0:{}}s:49:\"\0Gloudemans\\Shoppingcart\\CartItem\0associatedModel\";N;s:41:\"\0Gloudemans\\Shoppingcart\\CartItem\0taxRate\";i:21;}}}',NULL,NULL,NULL,NULL);
+INSERT INTO `shoppingcart` VALUES ('::1','default','O:29:\"Illuminate\\Support\\Collection\":1:{s:8:\"\0*\0items\";a:0:{}}',NULL,NULL,NULL,NULL),('ABCDrcO4duhgEg','default','O:29:\"Illuminate\\Support\\Collection\":1:{s:8:\"\0*\0items\";a:0:{}}',NULL,NULL,NULL,NULL),('ABCDSmUoASw4rX','default','O:29:\"Illuminate\\Support\\Collection\":1:{s:8:\"\0*\0items\";a:0:{}}',NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `shoppingcart` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -302,7 +255,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`Id`),
   UNIQUE KEY `User_Name_UNIQUE` (`email`),
   UNIQUE KEY `Id_UNIQUE` (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -311,7 +264,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'chris.redfield@gmail.com','$2y$10$3iR.2qkKgfh86eMtLNKUHe7o.U6fS9DiEPZLqvllE/hvU24I5ifrW','wcegzccrQdBLLJ91Fpv3IK14gyHq5LxXmBNCIuPZZ1xkA2wemmDjFhWbLfjW','Chris Redfield','2018-02-08 04:31:41','2018-02-08 04:31:41'),(2,'jill.valentine@hotmail.com','$2y$10$5y4dL6H7pm/yxqhtGOujueuxXlf3z33mEaILnpKhNxpWTlVQ/1QCe','3Fd9dBJfAa4SFG2n2Te5oV2MlR9mnrB1DIRKjOyiG4pzAyCt47KWZtJT8TtJ','Jill Valentine','2018-02-08 04:32:20','2018-02-08 04:32:20'),(3,'carlos.rivera@ymail.com','$2y$10$hyLoKAz/YvD6OgmlRVxZfeAsKvKtJd3.CZb2Xs/RUvRLVh8K9gKfy','bFYnwdRULETxbQnPUw3Ps3T6UTYq4cobRNvsmAdKPWthR61usW5MSFYxH5kc','Carlos Rivera','2018-02-08 04:32:50','2018-02-08 04:32:50'),(4,'Leon.kennedy@yahoo.com','$2y$10$rbU8kjYix7om0TBiqAGVnuN2vntfamaqZyusstR4X2U/t83pk9zWu','LNtCGnUS45Bfk5ARgNe0SwO7EyC6SH4vjBVAS4obMOXGJzoiGWbUb1RbwPQa','Leon Kennedy','2018-02-08 04:33:31','2018-02-08 04:33:31'),(5,'tifa.lockheart@gmail.com','$2y$10$P/phm3lZ8mqf7n1k3JWAi.G1/ui1VETXfYVuCsl0qLmhmh8X9D2hO','fs2QInkvymmSkdBApWMAL50OBMLDdFCZ4sHzBKFgvEhEk02KayGF5C61yxoL','Tifa Lockheart','2018-02-08 04:34:04','2018-02-08 04:34:04'),(6,'cloud.strife@yahoo.com','$2y$10$P4jYSFhygfU50juCc20VD.LA4owJWHACNmHmR9.XJqk0GlIY8sGPm','lf1df5i2i5SPF4qPYiFnqSIbyNjVJPNsmr61FZBsv8so8cx3y0g5zBQ1JCem','Cloud Stife','2018-02-08 04:34:36','2018-02-08 04:34:36'),(7,'barret.wallace@ymail.com','$2y$10$rFJq3SHDpAREZTxHOpAnje3qjNs6S2iZsdzqXGCeMkYFgLtNfgDJK','YnK4uZtCcz42YmAs3ysbOkMcwCVv8rVfXFAAzMegMid0Tgclf7zNxYXLyYlP','Barret Wallace','2018-02-08 04:35:17','2018-02-08 04:35:17'),(8,'cid.highwind@hotmail.com','$2y$10$8cJRgcZmJGQNoA9Hst3j3O8y1WNf8jaN6XOoi/8WKJqeR8HaeSIuC','sExuhBQYjLHm7F41kfYPtndctUaYwJLfOsTXAtJBR8gJGRrJYicJBCFkW2yA','Cid Highwind','2018-02-08 04:35:42','2018-02-08 04:35:42');
+INSERT INTO `users` VALUES (1,'chris.redfield@gmail.com','$2y$10$3iR.2qkKgfh86eMtLNKUHe7o.U6fS9DiEPZLqvllE/hvU24I5ifrW','wcegzccrQdBLLJ91Fpv3IK14gyHq5LxXmBNCIuPZZ1xkA2wemmDjFhWbLfjW','Chris Redfield','2018-02-08 04:31:41','2018-02-08 04:31:41'),(2,'jill.valentine@hotmail.com','$2y$10$5y4dL6H7pm/yxqhtGOujueuxXlf3z33mEaILnpKhNxpWTlVQ/1QCe','3Fd9dBJfAa4SFG2n2Te5oV2MlR9mnrB1DIRKjOyiG4pzAyCt47KWZtJT8TtJ','Jill Valentine','2018-02-08 04:32:20','2018-02-08 04:32:20'),(3,'carlos.rivera@ymail.com','$2y$10$hyLoKAz/YvD6OgmlRVxZfeAsKvKtJd3.CZb2Xs/RUvRLVh8K9gKfy','bFYnwdRULETxbQnPUw3Ps3T6UTYq4cobRNvsmAdKPWthR61usW5MSFYxH5kc','Carlos Rivera','2018-02-08 04:32:50','2018-02-08 04:32:50'),(4,'Leon.kennedy@yahoo.com','$2y$10$rbU8kjYix7om0TBiqAGVnuN2vntfamaqZyusstR4X2U/t83pk9zWu','LNtCGnUS45Bfk5ARgNe0SwO7EyC6SH4vjBVAS4obMOXGJzoiGWbUb1RbwPQa','Leon Kennedy','2018-02-08 04:33:31','2018-02-08 04:33:31'),(5,'tifa.lockheart@gmail.com','$2y$10$P/phm3lZ8mqf7n1k3JWAi.G1/ui1VETXfYVuCsl0qLmhmh8X9D2hO','fs2QInkvymmSkdBApWMAL50OBMLDdFCZ4sHzBKFgvEhEk02KayGF5C61yxoL','Tifa Lockheart','2018-02-08 04:34:04','2018-02-08 04:34:04'),(6,'cloud.strife@yahoo.com','$2y$10$P4jYSFhygfU50juCc20VD.LA4owJWHACNmHmR9.XJqk0GlIY8sGPm','lf1df5i2i5SPF4qPYiFnqSIbyNjVJPNsmr61FZBsv8so8cx3y0g5zBQ1JCem','Cloud Stife','2018-02-08 04:34:36','2018-02-08 04:34:36'),(7,'barret.wallace@ymail.com','$2y$10$rFJq3SHDpAREZTxHOpAnje3qjNs6S2iZsdzqXGCeMkYFgLtNfgDJK','YnK4uZtCcz42YmAs3ysbOkMcwCVv8rVfXFAAzMegMid0Tgclf7zNxYXLyYlP','Barret Wallace','2018-02-08 04:35:17','2018-02-08 04:35:17'),(8,'cid.highwind@hotmail.com','$2y$10$8cJRgcZmJGQNoA9Hst3j3O8y1WNf8jaN6XOoi/8WKJqeR8HaeSIuC','fRb4Dwo90TjoJ5J6ilyvxVx5tx9YSaUXuFJMPZcu6IpBLMQ3qWRo5bvkQuVW','Cid Highwind','2018-02-08 04:35:42','2018-02-08 04:35:42'),(9,'test@test.com','$2y$10$7FcfOO6lkul6Bn1DBTAtBu8G71FSNM9fYaKqyaFmh4KxisAyaEuSu','XWSbXsnXQrGHsIcQnIc9S1CxA420KY7itT9Fc3RHYrRun5tD1guOssoBpStg','test','2018-02-16 04:20:17','2018-02-16 04:20:17'),(10,'Testing@123.com','$2y$10$AA0oODW3rfkByk/iSQtvG.UWA44aN5WPF57oaMsxgaeZUJBxegTfC','D49YyqzP8MjWdGKmiPFz6ebeJ39HVsCpwPYiLLuinaf0kvVja2EqAI4Uhjpa','Testing','2018-02-17 18:33:20','2018-02-17 18:33:20');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -324,4 +277,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-15 22:02:21
+-- Dump completed on 2018-02-17 17:04:09
