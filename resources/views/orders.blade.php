923b6375c9be84c4a3ca0cb52325bfab34b9631b
Lacie Cumberland <Lacie@Bastet.ivalice.barrekroberts.com>
2018-02-20 21:13:14 -0600

    minor updates

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,12 +1,5 @@
 @extends('Template')
-
 @section('content')
-        	<!--- Body Area code goes here--->
-
-            <!--- End Body code --->
-
-
-
 <table class="table">
 	<thead>
 		<th scope="col">Order Id</th>
@@ -30,19 +23,10 @@
 			@else
 				<td>{{$order->customer->First_Name}} {{$order->customer->Last_Name}}</td>
 				<td>@if($order->Status=="Processing")<form method="post" action="loadOrder">{{ csrf_field() }}<input type="hidden" name="loadOrder" value=""><button name="orderId" value="{{$order->Id}}" type="submit">Modify Order</button></form>@endif</td>
-				
-					<td>@if($order->Status =='Processing')<form method="post" action="{{route('orderclose')}}">{{ csrf_field() }}<input type="hidden" name="_method" value="PUT"><button name="id" value="{{$order->Id}}" type="submit">Validate Order</button></form>@endif</td>
-				
-				
+				<td>@if($order->Status =='Processing')<form method="post" action="{{route('orderclose')}}">{{ csrf_field() }}<input type="hidden" name="_method" value="PUT"><button name="id" value="{{$order->Id}}" type="submit">Validate Order</button></form>@endif</td>
 				<td>@if($order->Status == 'Validated')<form method="post" action="{{route('orderclose')}}">{{ csrf_field() }}<input type="hidden" name="_method" value="PUT"><button name="id" value="{{$order->Id}}" type="submit">Close Order</button></form>@endif</td>
-				
 			@endif
 		</tbody>
 	@endforeach
 </table>
-
-
-<!--- End Body code --->
-
-
 @endsection
